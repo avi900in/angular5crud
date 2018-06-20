2ea7367bbf604e01631c26cea6dfb4999bb7b739
avi900in <40376287+avi900in@users.noreply.github.com>
2018-06-20 10:03:13 +0800

    Update DB.js

--- config/DB.js
+++ config/DB.js
@@ -1,5 +1,6 @@
 // DB.js
 
 module.exports = {
-   DB: 'mongodb://localhost:27017/angularcrud'
+   //DB: 'mongodb://localhost:27017/angularcrud'
+  DB: mongodb://<dbuser>:<dbpassword>@ds163680.mlab.com:63680/heroku_12f5gw2d/angulacrud
  };
